47afd281fa4df6676d06cf3674d30cfb8718fc3b
0xSolidity <88134314+0xSolidity@users.noreply.github.com>
2021-08-18 21:29:16 +0400

    Update farms.ts

--- src/config/constants/farms.ts
+++ src/config/constants/farms.ts
@@ -15,36 +15,6 @@ const farms: FarmConfig[] = [
     token: tokens.syrup,
     quoteToken: tokens.wbnb,
   },
-  {
-    pid: 1,
-    lpSymbol: 'SHIBCAKE-BNB LP',
-    lpAddresses: {
-      97: '0x4f2d19bbd600e4e88396c2f6177ef9f2bfa30671',
-      56: '0x4f2d19bbd600e4e88396c2f6177ef9f2bfa30671',
-    },
-    token: tokens.shibcake,
-    quoteToken: tokens.wbnb,
-  },
-  {
-    pid: 2,
-    lpSymbol: 'CAKE-BNB LP',
-    lpAddresses: {
-      97: '0x3ed8936cAFDF85cfDBa29Fbe5940A5b0524824F4',
-      56: '0x0eD7e52944161450477ee417DE9Cd3a859b14fD0',
-    },
-    token: tokens.cake,
-    quoteToken: tokens.wbnb,
-  },
-  {
-    pid: 3,
-    lpSymbol: 'BUSD-BNB LP',
-    lpAddresses: {
-      97: '',
-      56: '0x58F876857a02D6762E0101bb5C46A8c1ED44Dc16',
-    },
-    token: tokens.busd,
-    quoteToken: tokens.wbnb,
-  },
 ]
 
 export default farms
