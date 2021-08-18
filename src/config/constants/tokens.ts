71648d409d67eec120d3671fce6c50267857787c
0xSolidity <88134314+0xSolidity@users.noreply.github.com>
2021-08-18 21:14:45 +0400

    Update tokens.ts

--- src/config/constants/tokens.ts
+++ src/config/constants/tokens.ts
@@ -91,6 +91,15 @@ const tokens = {
     decimals: 18,
     projectLink: 'https://binamon.org/',
   },
+  safemoon: {
+    symbol: 'SAFEMOON',
+    address: {
+      56: '0x8076C74C5e3F5852037F31Ff0093Eeb8c8ADd8D3',
+      97: '',
+    },
+    decimals: 9,
+    projectLink: 'https://safemoon.net/',
+  },
   hero: {
     symbol: 'HERO',
     address: {
